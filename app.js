0034dde981869b857504b508a6290ed9888e4508
Chiheb abiza <142681693+chihebabiza@users.noreply.github.com>
2024-03-06 18:53:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// consoole.lgoasdljasdlkjklsj
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 // const sound = document.getElementById("sound");
